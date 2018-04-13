db3c9b2145751257b2f9117cdda2bc1d67eb2c47
David Leininger <davidbleininger@gmail.com>
2018-04-12 22:26:23 -0500

    Change Order of install

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # Get dotfiles installation directory
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+ln -sf "$DOTFILES_DIR/.aliases" ~
 ln -sf "$DOTFILES_DIR/.gitconfig" ~
-ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.gitconfig-dl" ~
 ln -sf "$DOTFILES_DIR/.gitconfig-wwt" ~
-ln -sf "$DOTFILES_DIR/.zshrc" ~
+ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.hyper.js" ~
-ln -sf "$DOTFILES_DIR/.aliases" ~
+ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/.z.sh" ~
