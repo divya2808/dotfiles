678ee6d178d3446fa5f67d657d90cee5ceb4d0b8
David Leininger <davidbleininger@gmail.com>
2018-04-18 15:12:43 -0500

    Alphabatize config settings - Because OCD...

--- .hyper.js
+++ .hyper.js
@@ -1,45 +1,16 @@
 module.exports = {
   config: {
-    // The update channel to receive updates from
-    updateChannel: 'stable',
-
-    // default font size in pixels for all tabs
-    fontSize: 16,
-
-    // font family with optional fallbacks
-    fontFamily: '"Operator Mono", "Inconsolata for Powerline", monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: "rgba(197,148,197,.8)",
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: "BLOCK",
-
-    // set to true for blinking cursor
-    cursorBlink: false,
-
-    // The text color under BLOCK cursor
-    cursorAccentColor: "#d8dee9",
-
-    // color of the text
-    foregroundColor: "#d8dee9",
-
+    // color of the terminal background
     backgroundColor: "#1b2b34",
+
+    // set to false for no bell
+    bell: "SOUND",
 
     // border color (window, tabs)
     borderColor: "#5fb3b3",
 
-    // Clicking on them Links
-    hyperlinks: {
-      clickAction: "open",
-      defaultBrowser: true
-    },
-
-    // custom css to embed in the main window
-    css: `li.tab-active{border-bottom: 1px solid #fac863 !important} .hyper_main{border-width: 2px !important;}`,
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: "12px 14px",
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -63,6 +34,39 @@ module.exports = {
       lightWhite: "#ffffff"
     },
 
+    // custom css to embed in the main window
+    css: `li.tab-active{border-bottom: 1px solid #fac863 !important} .hyper_main{border-width: 2px !important;}`,
+
+    // The text color under BLOCK cursor
+    cursorAccentColor: "#d8dee9",
+
+    // set to true for blinking cursor
+    cursorBlink: false,
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: "rgba(197,148,197,.8)",
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: "BLOCK",
+
+    // default font size in pixels for all tabs
+    fontSize: 16,
+
+    // font family with optional fallbacks
+    fontFamily: '"Operator Mono", "Inconsolata for Powerline", monospace',
+
+    // color of the text
+    foregroundColor: "#d8dee9",
+
+    // Clicking on them Links
+    hyperlinks: {
+      clickAction: "open",
+      defaultBrowser: true
+    },
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: "12px 14px",
+
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     shell: "/bin/zsh",
 
@@ -70,11 +74,8 @@ module.exports = {
     // by default ['--login'] will be used
     shellArgs: ["--login"],
 
-    // set to false for no bell
-    bell: "SOUND",
-
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
+    // The update channel to receive updates from
+    updateChannel: 'stable',
   },
 
   // a list of plugins to fetch and install from npm
