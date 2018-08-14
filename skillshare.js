2f4c8a39b4b966a7469c908c7d4bb47d7186bffd
David Leininger <davidbleininger@gmail.com>
2018-08-14 17:12:55 -0500

    Not pretty, but it works...

--- skillshare.js
+++ skillshare.js
@@ -1,7 +1,41 @@
+// FIRST STEP
 let courseTitle = document.querySelector('.class-details-header-title h2').innerHTML;
-let video = document.getElementById('vjs_video_3_html5_api');
+let video = document.getElementById('vjs_video_3_html5_api').getAttribute('src');
 let sessions = Array.from(document.querySelectorAll('.session-item'));
+let courseList = [];
 
+for (let i = 1; i < sessions.length; i++) {
+  setTimeout(function timer() {
+    sessions[i].click();
+    setTimeout(function timer() {
+      video = document.getElementById('vjs_video_3_html5_api').getAttribute('src');
+      let active = document.querySelector('.session-item.active');
+      let title = active.querySelector('.session-item-title').innerHTML;
+      title = title.replace(/\s+/g, '-');
+      courseList.push({
+        title: title,
+        link: video,
+      });
+    }, 1000);
+  }, i * 3000);
+}
+
+// download function
+function saveText(text, filename) {
+  var a = document.createElement('a');
+  a.setAttribute('href', 'data:text/plain;charset=utf-u,' + encodeURIComponent(text));
+  a.setAttribute('download', filename);
+  a.click()
+}
+
+// STEP TWO
+
+// give me that data in a json object!
+courseList = JSON.stringify(courseList)
+courseTitle = courseTitle.replace(/\s+/g, '-');
+saveText(courseList, courseTitle);
+
+// Not Needed as of now...
 // get me of all of the data!
 let course = sessions.map(session => {
   session.click();
