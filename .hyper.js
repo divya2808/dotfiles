afb1c52a6ab612d287ac248f8638f0abb8085a78
David Leininger <davidbleininger@gmail.com>
2019-05-23 06:20:33 -0500

    update terminal theme - New Tabs

--- .hyper.js
+++ .hyper.js
@@ -1,13 +1,23 @@
+const backgroundColor = '#1b2b34'
+const foregroundColor = '#d8dee9'
+const cursorColor = '#5fb3b3'
+const borderColor = '#1b2b34'
+const selectionColor = 'rgba(95, 179, 169, .3)'
+const tabNavBg = '#0f1c23'
+const tabText = '#65737e'
+const tabTextActive = '#d8dee9'
+const dividerBg = 'rgba(27,43,66,.4)'
+
 module.exports = {
   config: {
     // color of the terminal background
-    backgroundColor: "#1b2b34",
+    backgroundColor,
 
     // set to false for no bell
     bell: "SOUND",
 
     // border color (window, tabs)
-    borderColor: "#5fb3b3",
+    borderColor,
 
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false,
@@ -35,16 +45,59 @@ module.exports = {
     },
 
     // custom css to embed in the main window
-    css: `.hyper_main{border-width: 2px !important;}`,
+    css: `
+      #hyper {
+        border: 1px solid ${borderColor};
+      }
+      .tabs_nav {
+        background-color: ${tabNavBg};
+        border-bottom: 1px solid ${borderColor};
+      }
+      /* hide the tab border shim, we are providing our own */
+      .tabs_borderShim {
+        border-bottom-width: 0px;
+      }
+      /* Hide title when only one tab */
+      .tabs_title {
+  			background-color: ${backgroundColor};
+  		}
+      .tab_textInner {
+        color: ${tabText};
+        font-weight: 500;
+      }
+      .tab_tab{
+        border-left-color: ${tabNavBg} !important
+        border-left-width: 3px;
+      }
+      .tab_tab.tab_active {
+        background-color: ${backgroundColor};
+        border-left-color: ${cursorColor} !important;
+        border-left-width: 3px;
+      }
+      .tab_tab.tab_active .tab_textInner {
+        color: ${tabTextActive};
+      }
+      .tab_icon {
+        border-radius: 3px;
+        background: ${backgroundColor};
+        transition: all .2s;
+      }
+      .tab_icon:hover {
+        background: ${tabNavBg};
+      }
+      .splitpane_divider {
+        background-color: ${dividerBg} !important;
+      }
+    `,
 
     // The text color under BLOCK cursor
-    cursorAccentColor: "#d8dee9",
+    cursorAccentColor: foregroundColor,
 
     // set to true for blinking cursor
     cursorBlink: false,
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: "rgba(197,148,197,1)",
+    cursorColor,
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: "BLOCK",
@@ -56,7 +109,7 @@ module.exports = {
     fontFamily: '"Operator Mono", "Inconsolata for Powerline", monospace',
 
     // color of the text
-    foregroundColor: "#d8dee9",
+    foregroundColor,
 
     // Clicking on them Links
     hyperlinks: {
@@ -66,6 +119,8 @@ module.exports = {
 
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: "12px 14px",
+
+    selectionColor,
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     shell: "/bin/zsh",
